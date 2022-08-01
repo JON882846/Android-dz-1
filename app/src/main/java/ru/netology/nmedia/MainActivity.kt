77e24b98b93b7e1d59b3757568646327ae5e0f97
JON882846 <evgeniy.2888@gmail.com>
2022-08-01 11:20:05 +0500

    Android 2.2.1 two

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.netology.nmedia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity(){
 
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity(){
          content = "78",
          published = "555"
         )
-        post.likes = 1000
+        post.likes = 999999
 
         binding.like.setOnClickListener{
 
@@ -26,20 +27,15 @@ class MainActivity : AppCompatActivity(){
            val imageResult = if(post.likedByMe)
            {
                post.likes  ++
-
-
                R.drawable.ic_favorite_24dp
            }
            else {
                post.likes --
-
-
-
                R.drawable.ic_favorite_border_24dp
            }
             binding.like.setImageResource(imageResult)
-            binding.likeCount.text = post.likes.toString()
-       }
+            binding.likeCount.text = likeView(post.likes)
+        }
 
        binding.share.setOnClickListener{
             post.shares  = post.shares + 1
@@ -47,4 +43,15 @@ class MainActivity : AppCompatActivity(){
        binding.shareCount.text = post.shares.toString()
        }
    }
+}
+
+fun likeView (count:Int): String {
+
+   return  when(count){
+        in 0..999 ->"$count"
+        in 1000..1099 ->"${round((count/1000).toDouble())}K"
+        in 1100..10_000 ->"${round((count/100).toDouble())/10}K"
+        in 10001..999_999 ->"${round((count/1_000).toDouble())}K"
+        else -> "${round((count/10_000).toDouble())/10}M"
+    }
 }
