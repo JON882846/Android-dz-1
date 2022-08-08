c32087a3c1687250b365a73a09f9d142891aede2
JON882846 <evgeniy.2888@gmail.com>
2022-08-08 20:29:45 +0500

    Android 2.2.1 three

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -14,36 +14,38 @@ class MainActivity : AppCompatActivity(){
         setContentView(binding.root)
 
         val post = Post(
-         id = 0L,
-         author = "Неотология",
-         content = "78",
-         published = "555"
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false
         )
         post.likes = 999999
 
-        binding.like.setOnClickListener{
+        with(binding){
+         authorName.text = post.author
+         datepost.text = post.published
+         textpost.text = post.content
 
-           post.likedByMe =!post.likedByMe
-           val imageResult = if(post.likedByMe)R.drawable.ic_favorite_24dp else R.drawable.ic_favorite_border_24dp
-           if(post.likedByMe)post.likes  ++ else post.likes --
-           binding.like.setImageResource(imageResult)
-           binding.likeCount.text = likeView(post.likes)
+
+        binding.like.setOnClickListener {
+
+            post.likedByMe = !post.likedByMe
+            val imageResult =
+                if (post.likedByMe) R.drawable.ic_favorite_24dp else R.drawable.ic_favorite_border_24dp
+            if (post.likedByMe) post.likes++ else post.likes--
+            binding.like.setImageResource(imageResult)
+            binding.likeCount.text = likeView(post.likes)
         }
 
-       binding.share.setOnClickListener{
+        binding.share.setOnClickListener {
 
-           post.shares  = post.shares + 1
-       binding.shareCount.text = shareView(post.shares)
-       }
-   }
+            post.shares = post.shares + 1
+            binding.shareCount.text = shareView(post.shares)
+        }
+    }
+    }
 }
-
-
-
-
-
-
-
 
 fun likeView (count:Int): String {
 
